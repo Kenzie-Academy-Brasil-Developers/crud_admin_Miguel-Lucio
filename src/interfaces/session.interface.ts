8304198fbc40e443df10e8d3e24e66c1c3fd7ca9
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2023-10-19 21:35:33 -0300

    feat: functional login route

---
+++ src/interfaces/session.interface.ts
@@ -0,0 +1,5 @@
+import { z } from "zod";
+import { sessionSchema } from "../schemas/session.schema";
+
+export type TSessionCreate = z.infer<typeof sessionSchema>;
+export type TSessionReturn = { token: string };
