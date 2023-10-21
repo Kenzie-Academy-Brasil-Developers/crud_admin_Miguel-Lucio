a9060ca725cb519f9dfb368a3176cb1e2020c97b
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2023-10-20 23:17:11 -0300

    fix: TUserCourses

--- src/interfaces/users.interface.ts
+++ src/interfaces/users.interface.ts
@@ -1,6 +1,5 @@
 import { z } from "zod";
 import {
-  userCoursesSchema,
   userCreateSchema,
   userReadSchema,
   userReturnSchema,
@@ -10,7 +9,14 @@ import {
 import { QueryResult } from "pg";
 
 export type TUser = z.infer<typeof userSchema>;
-export type TUserCourses = z.infer<typeof userCoursesSchema>;
+export type TUserCourses = {
+  couseId: number;
+  courseName: string;
+  courseDescription: string;
+  userActiveInCourse: boolean;
+  userId: number;
+  userName: string;
+};
 
 export type TUserCreate = z.infer<typeof userCreateSchema>;
 export type TUserRead = z.infer<typeof userReadSchema>;
